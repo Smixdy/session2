27f40e296b297781e0e94287150344673ced48b9
Smixdy <145753558+Smixdy@users.noreply.github.com>
2025-02-06 08:51:52 +0300

    Update MyApp.kt

--- app/src/main/java/com/valance/oechappfinal/MyApp.kt
+++ app/src/main/java/com/valance/oechappfinal/MyApp.kt
@@ -13,8 +13,8 @@ class MyApp: Application() {
     override fun onCreate() {
         super.onCreate()
         supabaseClient = createSupabaseClient(
-            supabaseUrl = "https://tprnayptxrlhorhzhpos.supabase.co",
-            supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwcm5heXB0eHJsaG9yaHpocG9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAxNDE4NzQsImV4cCI6MjAyNTcxNzg3NH0.EqWuE_PSzQ0WMfHidLsH9YrdO8smvxd_OIhXTDbplMg"
+            supabaseUrl = "https://tprnayptxrdhsjwlwlwlhorhzhpos.supabase.co",
+            supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyjfjrjelwpwoe3847jrjdkdjdjdJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwcm5heXB0eHJsaG9yaHpocG9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAxNDE4NzQsImV4cCI6MjAyNTcxNzg3NH0.EqWuE_PSzQ0WMfHidLsH9YrdO8smvxd_OIhXTDbplMg"
         ) {
             install(Postgrest){
                 defaultSchema = "public"
